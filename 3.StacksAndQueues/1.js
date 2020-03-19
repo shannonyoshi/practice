1aae7107cc63383307b8042a2466b6b383e01506
Shannon <shannon.yoshikawa@gmail.com>
2020-03-19 17:28:19 -0400

    completed through ch.2 ex.5, added ch.3 exercises

---
+++ 3.StacksAndQueues/1.js
@@ -0,0 +1,3 @@
+/*
+Three in One: decribe how you could use a single array to implement 3 stacks
+*/
---
+++ 3.StacksAndQueues/2.js
@@ -0,0 +1,4 @@
+/*
+Stack Min: How would you design a stack which, in addition to push and pop has a function min which returns 
+the minimum element? Push, pop, and min shoulld all operate in o(1) time.
+*/
