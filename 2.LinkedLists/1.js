3082c4e2d44f702ab7b575a33052bba04b0d27ac
Shannon <shannon.yoshikawa@gmail.com>
2020-03-19 10:42:34 -0400

    completed exercise 2, reformatted previous exercises

--- 2.LinkedLists/1.js
+++ 2.LinkedLists/1.js
@@ -9,80 +9,79 @@ How would you solve this problem if a temporary buffer is not allowed?
 
 //basic linked list
 class Node {
-    constructor(data, next=null) {
-    this.data = data
-    this.next = next
-    }
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
 }
 
 class LinkedList {
-    constructor() {
-        this.head = null
+  constructor() {
+    this.head = null;
+  }
+  print() {
+    let current = this.head;
+    console.log("Linked List Data:");
+    while (current) {
+      console.log(current.data);
+      current = current.next;
     }
-    print() {
-        let current = this.head
-        console.log("Linked List Data:")
-        while (current) {
-            console.log(current.data)
-            current = current.next
-        }
-    }
+  }
 
-    addToTail (data) {
-        const newNode = new Node(data)
-        if (!this.head) {
-            this.head = newNode
+  addToTail(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let tail = this.head;
+      while (tail.next) {
+        tail = tail.next;
+      }
+      tail.next = newNode;
+    }
+  }
+  addToHead(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
+
+  remove(data) {
+    let nodeToDelete = this.head;
+    let prev = null;
+    while (nodeToDelete.data != data) {
+      prev = nodeToDelete;
+      nodeToDelete = nodeToDelete.next;
+    }
+    prev.next = nodeToDelete.next;
+  }
+  removeDuplicates() {
+    let slow = this.head;
+    while (slow) {
+      let fast = slow.next;
+      let prev = slow;
+      while (fast) {
+        if (slow.data === fast.data) {
+          prev.next = fast.next;
+          fast = fast.next;
         } else {
-            let tail = this.head
-            while(tail.next) {
-                tail = tail.next
-            }
-            tail.next = newNode
+          prev = fast;
+          fast = fast.next;
         }
-
+      }
+      slow = slow.next;
     }
-    addToHead (data) {
-        const newNode = new Node(data)
-        if (!this.head) {
-            this.head = newNode
-        } else {
-            newNode.next = this.head
-            this.head = newNode
-        }
-    }
-
-    remove (data) {
-        let nodeToDelete = this.head
-        let prev = null
-        while (nodeToDelete.data != data) {
-            prev = nodeToDelete
-            nodeToDelete = nodeToDelete.next
-        }
-        prev.next = nodeToDelete.next
-    }
-    removeDuplicates () {
-        let slow = this.head
-        while (slow) {
-            let fast = slow.next
-            let prev = slow
-            while (fast) {
-                if (slow.data ===fast.data) {
-                    prev.next = fast.next
-                    fast = fast.next
-                } else {
-                    prev = fast
-                    fast= fast.next
-                }
-            }
-            slow = slow.next
-        }
-    }
+  }
 }
 
-linkedList = new LinkedList()
-nodeData = [1,1,1,2,3,4,5,6,7,8,9,10,3,6]
-for (let i=0; i< nodeData.length; i++) {
-    linkedList.addToTail(nodeData[i])
+linkedList = new LinkedList();
+nodeData = [1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 3, 6];
+for (let i = 0; i < nodeData.length; i++) {
+  linkedList.addToTail(nodeData[i]);
 }
-linkedList.removeDuplicates()
-linkedList.print()
+linkedList.removeDuplicates();
+linkedList.print();
--- 2.LinkedLists/2.js
+++ 2.LinkedLists/2.js
@@ -1,3 +1,73 @@
 /*
 Return Kth to Last: Implement an algorithm to find the Kth to last element in a singly linked list.
 */
+
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  print() {
+    let current = this.head;
+    console.log("Linked List Data:");
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+
+  addToTail(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let tail = this.head;
+      while (tail.next) {
+        tail = tail.next;
+      }
+      tail.next = newNode;
+    }
+  }
+  addToHead(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
+
+  getLength() {
+    let length = 0;
+    let current = this.head;
+    while (current) {
+      length += 1;
+      current = current.next;
+    }
+    return length
+  }
+
+  returnKthFromEnd(k) {
+    let target = this.getLength()-k
+    let current = this.head
+    while (target > 0) {
+        current = current.next
+        target-=1
+    }
+    return current
+  }
+}
+
+linkedList = new LinkedList();
+nodeData = [1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 3, 6];
+for (let i = 0; i < nodeData.length; i++) {
+  linkedList.addToTail(nodeData[i]);
+}
+let node = linkedList.returnKthFromEnd(3)
