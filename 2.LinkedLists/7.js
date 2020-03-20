3c146bd3b6d9aeddfdfa78615a8b73d5dd27bc6e
Shannon <shannon.yoshikawa@gmail.com>
2020-03-20 10:32:41 -0400

    completed ch2 exercises

--- 2.LinkedLists/7.js
+++ 2.LinkedLists/7.js
@@ -6,3 +6,99 @@ That is, if the kth node of the first linked list is the exact same node (by ref
 jth node of the second linked list, then they are intersecting.
 
 */
+
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  print(additionalDescription = "") {
+    let current = this.head;
+    let values = [];
+    while (current) {
+      values.push(current.data);
+      current = current.next;
+    }
+    console.log(`${additionalDescription} LinkedList Data: ${values}`);
+  }
+
+  addToTail(newNode) {
+    // const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let tail = this.head;
+      while (tail.next) {
+        tail = tail.next;
+      }
+      tail.next = newNode;
+    }
+  }
+  addToHead(newNode) {
+    // const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
+
+  getLength() {
+    let length = 0;
+    let current = this.head;
+    while (current) {
+      length += 1;
+      current = current.next;
+    }
+    return length;
+  }
+}
+
+const findIntersection = (linkedlist1, linkedlist2) => {
+  console.log("find intersection");
+  let current1 = linkedlist1.head;
+  while (current1) {
+    let current2 = linkedlist2.head;
+    while (current2) {
+      if (current1 === current2) {
+        return current1;
+      }
+      current2 = current2.next;
+    }
+    current1 = current1.next;
+  }
+  return false;
+};
+
+linkedList = new LinkedList();
+linkedList2 = new LinkedList();
+nodeData = [1, 1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1, 1];
+for (let i = 0; i < nodeData.length; i++) {
+  let newNode = new Node(nodeData[i]);
+  linkedList.addToTail(newNode);
+  console.log("first for loop");
+  if (i === 5) {
+    console.log("i=5");
+    const nodeData2 = [1, 2, 3, 4, 5, 6, 7];
+    for (let j = 0; j < nodeData2.length; j++) {
+      console.log("second for loop");
+      let newNode2 = new Node(nodeData2[i]);
+      console.log("newNode2", newNode2);
+      linkedList2.addToTail(newNode2);
+      if ((j = 6)) {
+        console.log("j=4");
+        linkedList2.addToTail(newNode);
+      }
+    }
+  }
+}
+
+result = findIntersection(linkedList, linkedList2);
+console.log(result);
--- 2.LinkedLists/8.js
+++ 2.LinkedLists/8.js
@@ -1,10 +1,107 @@
 /*
-Loop Detection: Given a circular linked list, implement an algorithm that eturns the node at the beginning of the loop.
+Loop Detection: Given a circular linked list, implement an algorithm that returns the node at the beginning of the loop.
 
 DEFINITION
-Circular linked list: A (corrupt) linked list in which a node's next pointer points to an earlier node, so as to make a loop in the linked list.
+Circular linked list: A (corrupt) linked list in which a node's next pointer points to an earlier node, 
+so as to make a loop in the linked list.
 
 EXAMPLE
 Input: A -> B -> C -> D -> E -> C[the same C as earlier]
 Output: C
 */
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  print(additionalDescription = "") {
+    let current = this.head;
+    let values = [];
+    while (current) {
+      values.push(current.data);
+      current = current.next;
+    }
+    console.log(`${additionalDescription} LinkedList Data: ${values}`);
+  }
+
+  addToTail(newNode) {
+    // console.log("addToTail: ", newNode);
+    // const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let tail = this.head;
+      while (tail.next) {
+        tail = tail.next;
+      }
+      tail.next = newNode;
+    }
+  }
+  addToHead(newNode) {
+    // const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
+
+  getLength() {
+    let length = 0;
+    let current = this.head;
+    while (current) {
+      length += 1;
+      current = current.next;
+    }
+    return length;
+  }
+}
+
+const loopDetection = linkedList => {
+  let slow = linkedList.head;
+  let fast = linkedList.head.next;
+  let i = 0;
+  while (slow) {
+    if (fast === slow) {
+      return fast;
+    }
+    if (i % 2 == 0) {
+      slow = slow.next;
+    }
+    fast = fast.next;
+    i += 1;
+  }
+  return false;
+};
+
+const linkedList = new LinkedList();
+nodeData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let specialNode = {};
+let newNode = {};
+for (let i = 0; i < nodeData.length; i++) {
+  if (i != 9) {
+    newNode = new Node(nodeData[i]);
+    linkedList.addToTail(newNode);
+  }
+  if (i === 7) {
+    specialNode = newNode;
+  }
+  if (i === 9) {
+    newNode = new Node(nodeData[i], specialNode);
+    linkedList.addToTail(newNode);
+  }
+}
+
+result = loopDetection(linkedList);
+if (result) {
+  console.log(result.data);
+} else {
+  console.log(result);
+}
